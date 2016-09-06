e0f5c6cdd609813168e018b47ac71a0bc6617c6e
rutujabudukh28 <rutuja.budukh@cumminscollege.in>
2016-09-06 14:41:30 +0530

    Multiplication added

--- calculator.c
+++ calculator.c
@@ -8,7 +8,15 @@ int add(int p, int q)
 {
   return p+q;
 }
+
+//This function gives subtraction of two numbers
 int subtract(int p, int q)
 {
   return p-q;
+}
+
+//This function gives multiplication of two numbers.
+int mul(int p, int q)
+{
+  return p*q;
 }
